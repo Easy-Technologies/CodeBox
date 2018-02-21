cc12875eff9cc4ef7d9cd436a936e40c685a747c
jackmaster110 <dspfgh34@gmail.com>
2018-02-21 18:30:16 -0500

    Use this to install the program!
    This is the non-exe version. The exe is currently under development.

---
+++ codeBoxInstaller.bat
@@ -0,0 +1,2 @@
+bitsadmin.exe /transfer "Downloading batch file" https://raw.githubusercontent.com/Easy-Technologies/CodeBox/master/CodeBox/codeBox.bat C:\Program Files\CodeBox\codeBox.bat
+bitsadmin.exe /transfer "Downloading default directories file" https://raw.githubusercontent.com/Easy-Technologies/CodeBox/master/CodeBox/User%20Files/dirs.txt C:\Program Files\CodeBox\User Files\dirs.txt
